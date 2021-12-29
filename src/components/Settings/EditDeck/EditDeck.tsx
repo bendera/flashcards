1c3be1bdf32c92357ba8890b3ee0b49bb1135233
bendera <szoftverhiba@gmail.com>
2021-12-29 20:45:28 +0100

    refactor: EditDeck refactor

--- src/components/Settings/EditDeck/EditDeck.tsx
+++ src/components/Settings/EditDeck/EditDeck.tsx
@@ -1,32 +1,12 @@
-import { FC, useEffect, useRef, useState } from 'react';
+import { FC, useEffect } from 'react';
 import { Button, EditableText } from '@blueprintjs/core';
-import { nanoid } from 'nanoid';
 
-import { FlashCard } from 'types';
-import { useAppDispatch } from 'app/hooks';
-import { saveDeck } from 'features/deck/deckSlice';
-import FlashcardsAPI, { DeckCatalogItem } from 'utils/FlashcardsAPI';
+import { DeckCatalogItem } from 'utils/FlashcardsAPI';
 
 import ImportCards from './ImportCards/ImportCards';
 import CardItem from './CardItem.tsx/CardItem';
-import { updateCatalogItem } from 'features/deckCatalog/deckCatalogSlice';
+import useEditDeck from './useEditDeck';
 import styles from './EditDeck.module.css';
-
-const createAnEmptyCard = (): FlashCard => {
-  return {
-    id: nanoid(),
-    frontSide: '',
-    backSide: '',
-  };
-};
-
-interface DeckMetaData {
-  cardsByBoxes: {
-    [key: string]: number;
-  };
-  drawCounter: number;
-  sessionCounter: number;
-}
 
 interface EditDecksProps {
   deckToEdit?: DeckCatalogItem;
@@ -35,102 +15,20 @@ interface EditDecksProps {
 const EditDeck: FC<EditDecksProps> = ({
   deckToEdit = { id: '', title: '', active: 0 },
 }) => {
-  const { active, id, title } = deckToEdit;
-  const emptyCard = createAnEmptyCard();
-  const dispatch = useAppDispatch();
-  const [deckTitle, setDeckTitle] = useState(title);
-  const [cards, setCards] = useState<FlashCard[]>([emptyCard]);
-  const deckMetaDataRef = useRef<DeckMetaData>({
-    cardsByBoxes: {
-      [emptyCard.id]: 1,
-    },
-    drawCounter: 0,
-    sessionCounter: 0,
-  });
-
-  const handleAddCardClick = () => {
-    const newCard = createAnEmptyCard();
-
-    setCards([...cards, newCard]);
-    deckMetaDataRef.current.cardsByBoxes[newCard.id] = 1;
-  };
-
-  const handleImport = (imported: FlashCard[]) => {
-    setCards([...cards, ...imported]);
-
-    imported.forEach((c) => {
-      deckMetaDataRef.current.cardsByBoxes[c.id] = 1;
-    });
-  };
-
-  const handleCardItemChange = (card: FlashCard) => {
-    const newCards = cards.map((c) => {
-      if (c.id !== card.id) {
-        return c;
-      }
-
-      return card;
-    });
-
-    setCards(newCards);
-  };
-
-  const handleSave = async () => {
-    const { cardsByBoxes, drawCounter, sessionCounter } =
-      deckMetaDataRef.current;
-
-    await dispatch(
-      updateCatalogItem({
-        id,
-        title: deckTitle,
-        active,
-      })
-    );
-    await dispatch(
-      saveDeck({
-        id,
-        cards,
-        cardsByBoxes,
-        drawCounter,
-        sessionCounter,
-        title: deckTitle,
-      })
-    );
-  };
-
-  const handleTitleChange = (value: string) => {
-    setDeckTitle(value);
-  };
+  const {
+    cards,
+    deckTitle,
+    fetchDeck,
+    handleAddCardClick,
+    handleCardItemChange,
+    handleImport,
+    handleSave,
+    handleTitleChange,
+  } = useEditDeck(deckToEdit);
 
   useEffect(() => {
-    const fetchDeck = async () => {
-      const api = new FlashcardsAPI();
-      const res = await api.getDeck(deckToEdit.id);
-
-      if (res.data) {
-        const { cards, cardsByBoxes, drawCounter, sessionCounter, title } =
-          res.data;
-
-        deckMetaDataRef.current = {
-          cardsByBoxes,
-          drawCounter,
-          sessionCounter,
-        };
-        setDeckTitle(title);
-        setCards(cards);
-      } else {
-        const cardsByBoxes: { [key: string]: number } = {};
-
-        cards.forEach(({ id }) => {
-          cardsByBoxes[id] = 1;
-        });
-
-        deckMetaDataRef.current.cardsByBoxes = cardsByBoxes;
-      }
-    };
-
     fetchDeck();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [deckToEdit]);
 
   return (
---
+++ src/components/Settings/EditDeck/useEditDeck.ts
@@ -0,0 +1,131 @@
+import { useRef, useState } from 'react';
+import { nanoid } from 'nanoid';
+import { FlashCard } from 'types';
+import { useAppDispatch } from 'app/hooks';
+import FlashcardsAPI, { DeckCatalogItem } from 'utils/FlashcardsAPI';
+import { updateCatalogItem } from 'features/deckCatalog/deckCatalogSlice';
+import { saveDeck } from 'features/deck/deckSlice';
+
+const createAnEmptyCard = (): FlashCard => {
+  return {
+    id: nanoid(),
+    frontSide: '',
+    backSide: '',
+  };
+};
+
+interface DeckMetaData {
+  cardsByBoxes: {
+    [key: string]: number;
+  };
+  drawCounter: number;
+  sessionCounter: number;
+}
+
+const useEditDeck = (deckToEdit: DeckCatalogItem) => {
+  const { active, id, title } = deckToEdit;
+  const emptyCard = createAnEmptyCard();
+  const dispatch = useAppDispatch();
+  const [deckTitle, setDeckTitle] = useState(title);
+  const [cards, setCards] = useState<FlashCard[]>([emptyCard]);
+  const deckMetaDataRef = useRef<DeckMetaData>({
+    cardsByBoxes: {
+      [emptyCard.id]: 1,
+    },
+    drawCounter: 0,
+    sessionCounter: 0,
+  });
+
+  const fetchDeck = async () => {
+    const api = new FlashcardsAPI();
+    const res = await api.getDeck(deckToEdit.id);
+
+    if (res.data) {
+      const { cards, cardsByBoxes, drawCounter, sessionCounter, title } =
+        res.data;
+
+      deckMetaDataRef.current = {
+        cardsByBoxes,
+        drawCounter,
+        sessionCounter,
+      };
+      setDeckTitle(title);
+      setCards(cards);
+    } else {
+      const cardsByBoxes: { [key: string]: number } = {};
+
+      cards.forEach(({ id }) => {
+        cardsByBoxes[id] = 1;
+      });
+
+      deckMetaDataRef.current.cardsByBoxes = cardsByBoxes;
+    }
+  };
+
+  const handleAddCardClick = () => {
+    const newCard = createAnEmptyCard();
+
+    setCards([...cards, newCard]);
+    deckMetaDataRef.current.cardsByBoxes[newCard.id] = 1;
+  };
+
+  const handleImport = (imported: FlashCard[]) => {
+    setCards([...cards, ...imported]);
+
+    imported.forEach((c) => {
+      deckMetaDataRef.current.cardsByBoxes[c.id] = 1;
+    });
+  };
+
+  const handleCardItemChange = (card: FlashCard) => {
+    const newCards = cards.map((c) => {
+      if (c.id !== card.id) {
+        return c;
+      }
+
+      return card;
+    });
+
+    setCards(newCards);
+  };
+
+  const handleSave = async () => {
+    const { cardsByBoxes, drawCounter, sessionCounter } =
+      deckMetaDataRef.current;
+
+    await dispatch(
+      updateCatalogItem({
+        id,
+        title: deckTitle,
+        active,
+      })
+    );
+    await dispatch(
+      saveDeck({
+        id,
+        cards,
+        cardsByBoxes,
+        drawCounter,
+        sessionCounter,
+        title: deckTitle,
+      })
+    );
+  };
+
+  const handleTitleChange = (value: string) => {
+    setDeckTitle(value);
+  };
+
+  return {
+    cards,
+    deckTitle,
+    fetchDeck,
+    handleAddCardClick,
+    handleCardItemChange,
+    handleImport,
+    handleSave,
+    handleTitleChange,
+  };
+};
+
+export default useEditDeck;
